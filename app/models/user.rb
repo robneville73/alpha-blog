ab36125ac590ee2b56a923a39fe9f428a105e09e
Rob Neville <rn@retailarchitects.com>
2017-11-02 21:15:20 -0400

    add secure password and attr to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,4 +15,6 @@ class User < ApplicationRecord
   has_many :articles
 
   before_save { self.email = email.downcase }
+
+  has_secure_password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171102213750) do
+ActiveRecord::Schema.define(version: 20171103011123) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20171102213750) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
