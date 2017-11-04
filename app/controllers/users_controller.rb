e84c24a35fd4d1fa2b94801933757d7ab8baa838
Rob Neville <rn@retailarchitects.com>
2017-11-04 11:11:09 -0400

    fix login at user create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,8 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash[:success] = "Welcome to AlphaBlog #{@user.username}"
-      redirect_to articles_path
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
     else
       render 'new'
     end
